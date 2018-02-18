065453495c9de4dfc941e80da7a2bacf08941d23
longtime1116 <longtime1116@gmail.com>
2018-02-19 08:45:51 +0900

    use params

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/json'
 
 get '/' do
-  'hello'
+  'hello world'
+end
+
+get "/:name" do
+  data = {name: params[:name]}
+  json data
 end
