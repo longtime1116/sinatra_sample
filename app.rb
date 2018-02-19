437b636a49dbf22c53f8803d2b638e9390ef56d0
longtime1116 <longtime1116@gmail.com>
2018-02-19 09:03:45 +0900

    search now

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/json'
+#require 'net/http'
+require 'open-uri'
+#require 'pry'
 
 get '/' do
   'hello world'
 end
 
 get "/:name" do
-  data = {name: params[:name]}
-  json data
+  #data = Net::HTTP.get_print(URI.parse("https://github.com/users/#{params[:name]}/contributions"))
+
+  data = open("https://github.com/users/#{params[:name]}/contributions")
+#  p "hoge"
+#  p data
+
+#  binding.pry
+  return data
 end
